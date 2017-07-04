774e87b07cb0993442f03121f0490359be8eea85
Hien Pham <hien.pham@ibebiz.com>
2017-07-04 23:19:30 +0700

    Add project files.

---
+++ ISchool/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ISchool.Models;
+
+namespace ISchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        ISchoolEntities db = new ISchoolEntities();
+       
+        public ActionResult Dashbroad()
+        {
+            return View();
+        }
+    }
+}
